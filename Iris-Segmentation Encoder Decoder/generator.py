11542c022d6fbfba33eb35a7944c70abc06ebadd
ataata107 <32903329+ataata107@users.noreply.github.com>
2018-08-04 02:18:23 +0530

    training comp

---
+++ Iris-Segmentation Encoder Decoder/generator.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+from keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
+from scipy.misc import imresize
+
+def data_gen_small(img_dir, mask_dir, lists, batch_size, dims, n_labels):
+    while True:
+        #ix = np.random.choice(np.arange(len(lists)), batch_size)
+        ix = batch_size
+        imgs = []
+        labels = []
+        for i in ix:
+            # images
+            original_img = cv2.imread(img_dir + lists.iloc[i, 0]+".jpg")[:, :, ::-1]
+            resized_img = cv2.resize(original_img, dims+[3])
+            array_img = img_to_array(resized_img)/255
+            imgs.append(array_img)
+            # mask
+            original_mask = cv2.imread(mask_dir + lists.iloc[i, 0] + '.png')
+            resized_mask = cv2.resize(original_mask, (dims[0], dims[1]))
+            array_mask = catelab(resized_mask[:, :, 0], dims, n_labels)
+            labels.append(array_mask)
+        imgs = np.array(imgs)
+        labels = np.array(labels)
+        yield imgs, labels
+
+
+def catelab(labels, dims, n_labels):
+    x = np.zeros([dims[0], dims[1], n_labels])
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            x[i, j, labels[i][j]]=1
+    x = x.reshape(dims[0] * dims[1], n_labels)
+    return x
--- Iris-Segmentation Encoder Decoder/train.py
+++ Iris-Segmentation Encoder Decoder/train.py
@@ -5,13 +5,31 @@ from keras.layers import Dense, Activation, Flatten
 from keras.layers import merge, Input
 from keras.models import Model
 from keras.preprocessing import image
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
+from keras.optimizers import SGD
 from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D
 from keras.applications.imagenet_utils import preprocess_input
 from vgg16 import VGG16
 #encoder decoder
-
-image_input = Input(shape=(256, 256, 3))
-model = VGG16(input_tensor=image_input, include_top=False,input_shape=(256,256,3),weights='imagenet')
+epoch_steps = int(200/1)
+val_steps = int(50/1)
+image_input = Input(shape=(480, 360, 3))
+model = VGG16(input_tensor=image_input, include_top=False,input_shape=(480,360,3),weights='imagenet')
 model.summary()
 
+train_list = pd.read_csv(args.train_list,header=None)
+val_list = pd.read_csv(args.val_list,header=None)
+
+trainimg_dir = './training_dataset'
+trainmsk_dir = './training_mask'
+valimg_dir = './validating_dataset'
+valmsk_dir = './validating_mask'
+
+train_gen = data_gen_small(trainimg_dir, trainmsk_dir, train_list, batch_size=1, [480, 360], n_labels =2)
+val_gen = data_gen_small(valimg_dir, valmsk_dir, val_list, batch_size=1, [480, 360], n_labels =2)
+stochastic = SGD(lr=0.001, momentum=0.9, decay=0.0, nesterov=False)
+callbacks = [ModelCheckpoint('bestmodel.h5', monitor='val_loss', save_best_only=True)]
+
+model.compile(loss="categorical_crossentropy", optimizer=stochastic, metrics=["accuracy"])
+segnet.fit_generator(train_gen, steps_per_epoch=epoch_steps, epochs=200, validation_data=val_gen, validation_steps=val_steps,callbacks=callbacks)
 
